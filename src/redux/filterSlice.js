7c19dc506891966a42af05a78baeb852e08eca44
R3enox <129979133+R3enox@users.noreply.github.com>
2023-12-08 20:38:11 +0200

    add filterComment

---
+++ src/redux/filterSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  filter: "",
+};
+
+export const filterSlice = createSlice({
+  name: "filter",
+  initialState,
+  reducers: {
+    filterComment: (state, { payload }) => {
+      state.filter = payload;
+    },
+  },
+});
+
+export const { filterComment } = filterSlice.actions;
+
+export default filterSlice.reducer;
+
+//Selectors
+
+export const selectFilter = (state) => state.filter.filter;
